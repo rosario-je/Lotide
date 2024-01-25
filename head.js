46081bb6435576d4ba2cb0f82b49c79bda40a773
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-01-25 13:27:35 -0500

    Created head.js and added function

---
+++ head.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return (`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(text) {
+  return text [0];
+}
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
+assertEqual(true, false);
+assertEqual(true, true);
+assertEqual(false, false);
