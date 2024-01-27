79070397555cb98e81336c698e81d701cb2bb2e8
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-01-27 14:10:59 -0500

    Implemented function

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } if (arr1.length !== arr2.length) {
+      return false;
+    }
+    return true;
+  }
+};
+
+
+const assertEqual = function(actual, expected) {
+  //Write your function here...
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+ 
