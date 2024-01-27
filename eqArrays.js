2ff3bc79a27e94c978d9d9fe5ebc3bc65567b3c0
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-01-27 14:34:30 -0500

    Fixed function according to AI recommendations

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,14 @@
 const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-    } if (arr1.length !== arr2.length) {
-      return false;
     }
-    return true;
+    
   }
+  return true;
 };
 
 
