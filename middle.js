f09f5899fa70cfa65e8f102a9c4aa284edf9a167
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-01-28 01:15:42 -0500

    Created function

---
+++ middle.js
@@ -0,0 +1,44 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  // Call eqArrays and print the appropriate message based on the result
+  let check = eqArrays(arr1, arr2);
+  if (check === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = (array) => {
+  if (array.length === 1 || array.length === 2){
+    return []
+  }else if (array.length % 2 == 1){
+    const middle = Math.floor(array.length / 2)
+    const value = array[middle];
+    return value;
+  } else if (array.length % 2 == 0){
+    const firstNum = Math.floor(array.length / 2)
+    const secondNum = (array.length / 2) + 1
+    return [firstNum, secondNum]
+  }
+}
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []);
+
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
