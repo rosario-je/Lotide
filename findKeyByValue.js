241f9626d0cc582ade37f93cdc600de5307ad840
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-01-30 20:46:12 -0500

    Implemented function & test cases

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//iterate through all the keys in the object
+//if the given key has the same value as the given value, return the key
+
+
+const findKeyByValue = (object, value) => {
+  for (const genre in object){
+    const movie = object[genre];
+    if (movie === value){
+      return genre;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
