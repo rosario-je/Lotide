a714f09f793a8f125de4960cb7fcc4f858a90423
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-01-30 22:46:51 -0500

    Completed function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,39 @@
+const eqObjects = (object1, object2) => {
+  //Creates a variable with an array containing all the keys in given object
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  //Checks if the length of the array is not the same to return false
+  if (keys1.length !== keys2.length) {
+    return false;
+  } else {
+    //Checks for every key in both objects and compares them, if they are not the same, return false
+    for (const key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+  
+    return true;
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  // Implement me!
+  const inspect = require('util').inspect;
+  let check = eqObjects(object1, object2);
+  if (check === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  console.log(`Example label: ${inspect(actual)}`);
+};
+
