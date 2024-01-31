37917885f8adfde479b89b0cf936e5463e054a29
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-01-30 23:04:08 -0500

    Fixed logs

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -30,9 +30,9 @@ const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   let check = eqObjects(actual, expected);
   if (check === true) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`🟢🟢🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
   console.log(`Example label: ${inspect(actual)}`);
 };
