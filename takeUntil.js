b84d205e59964426be5fb7d9b3f8a26033bccbcc
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-01 20:52:57 -0500

    Organized the code

--- takeUntil.js
+++ takeUntil.js
@@ -5,18 +5,25 @@ const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 
 const takeUntil = function(array, callback) {
-  const result = []
+  const result = [];
   //For every item in the array
-  for (const number of array){
+  for (const number of array) {
     //the callback function returns true for this number, return the results array
-    if (callback(number)){
+    if (callback(number)) {
       return result;
     }
-    //If the callback returns false, push said number **LOOK AT CALLBACK FUNCTION 
-    result.push(number)
+    //If the callback returns false, push said number **LOOK AT CALLBACK FUNCTION
+    result.push(number);
   }
   return result;
-}
+};
+
+/*CALLBACK FUNCTION*/
+//Call initial function to run through every item in data1, return true if the number is less than 0
+const results1 = takeUntil(data1, (item) => {
+  return item < 0;
+});
+
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
@@ -40,11 +47,7 @@ const assertArraysEqual = (arr1, arr2) => {
   }
 };
 
-/*CALLBACK FUNCTION*/
-//Call initial function to run through every item in data1, return true if the number is less than 0
-const results1 = takeUntil(data1, (item) => {
-  return item < 0
-});
+
 
 const results2 = takeUntil(data2, x => x === ',');
 
@@ -53,5 +56,5 @@ console.log('---');
 console.log(results2);
 
 
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
-assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ])
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
