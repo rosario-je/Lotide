87281d5655d86aa42d0a4aedd8bf4c2525b8a1d6
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-01 21:35:34 -0500

    Started implementing function

---
+++ findKey.js
@@ -0,0 +1,26 @@
+// Assert Equal
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const findKey = (object, callback) => {
+  for (const key in object){
+    if (callback(key)){
+      return key;
+    }
+  }
+};
+
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
