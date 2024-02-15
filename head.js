76b55bc8199cb5d125da13411d19919c24f7e0dc
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-15 00:37:49 -0500

    Created test file for head.js

--- head.js
+++ head.js
@@ -5,7 +5,5 @@ const head = function(array) {
   return array[0];
 }
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-
+module.exports = head
 
