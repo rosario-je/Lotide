e29a9d3ae64777b9b04c3f13815698a5d49b065e
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-15 00:29:17 -0500

    Refactored head.js

--- head.js
+++ head.js
@@ -1,19 +1,15 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return (`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
-  return text [0];
+  return array[0];
 }
 
 console.log(assertEqual(head([5,6,7]), 5));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
 
-assertEqual(true, false);
-assertEqual(true, true);
-assertEqual(false, false);
+// assertEqual(true, false);
+// assertEqual(true, true);
+// assertEqual(false, false);
+
+
