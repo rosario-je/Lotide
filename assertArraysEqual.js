7b5935659e94c9d182d828ba8f958edab9dd8882
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-15 01:04:27 -0500

    Improved syntax

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,7 +3,7 @@ const eqArrays = require('./eqArrays')
 const assertArraysEqual = (arr1, arr2) => {
   // Call eqArrays and print the appropriate message based on the result
   let check = eqArrays(arr1, arr2);
-  if (check === true) {
+  if (check) {
     console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
