48336bb0c5283cfaee0e17f817b49cb0944122a8
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-15 00:51:28 -0500

    File: Created tailTest.js and refactored tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return (`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const tail = (array) => {
   return array.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2));
-console.log(assertEqual(result[0], "Lighthouse"));
-console.log(assertEqual(result[1], "Labs"));
+module.exports = tail;
