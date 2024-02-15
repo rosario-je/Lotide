2eea4197a166c181fe8d1c82bbaa9486604646e7
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-15 00:58:39 -0500

    File: Refactored eqArrays.js File: Created test file for eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -9,22 +9,4 @@ const eqArrays = (arr1, arr2) => {
   }
   return true;
 };
-
-
-const assertEqual = function(actual, expected) {
-  //Write your function here...
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
- 
+module.exports = eqArrays;
